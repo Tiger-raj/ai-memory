36dbf052af3942265d94be54dbf8e4b1db4901af
Tiger-raj <rajbajpai2004@gmail.com>
2025-06-06 09:17:19 +0530

    started with frontend, designed button component

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,32 @@
+interface ButtonProps {
+  variant: "primary" | "secondary";
+  size: "sm" | "md" | "lg";
+  text: string;
+  onClick: () => void;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+}
+
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-300 text-purple-500",
+};
+
+const sizeStyles = {
+  sm: "px-2 py-1 text-sm",
+  md: "px-4 py-2 text-md",
+  lg: "px-6 py-3 text-lg",
+};
+
+const defaultStyles = "m-2 rounded-md flex";
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button className={`${variantStyles[props.variant]} ${sizeStyles[props.size]} ${defaultStyles}`} onClick={props.onClick}>
+      {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null}
+      {props.text}
+    </button>
+  );
+};
+
+<Button variant="primary" size="md" text="Click Me" onClick={() => console.log("Button clicked")} />;
